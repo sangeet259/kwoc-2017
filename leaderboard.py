c3c03dfe1bc17d4e440403ce9c68e4435c9912ff
sipah00 <shubhamsipah00@gmail.com>
2017-08-10 00:40:39 +0530

    added sorting function

--- leaderboard.py
+++ leaderboard.py
@@ -1,7 +1,8 @@
-import requests
-import traceback 
-import json 
-import os
+#import requests
+#import traceback 
+#import json 
+#import os
+from students import *
 
 def get_commits(username, repo_name):
     '''
@@ -54,5 +55,53 @@ def slack_notification(message):
         if r.status_code != 200:
                 print("in slack_notification : {}".format(r.status_code))
                 print(r.text)
+
+
+def students_list():
+    #get data from data base and make object of students class and append in a list
+    alldata = [["dibya","koss",10,20,1000,13,15],["shubham","cricFev",7,23,100000,13,19],["americast","kwoc",10,15,150001,18,19],["rahul","krssg",23,56,33,10000,58],["mehul","robocup",18,52,56984,54,78],["saurabh","bhoot",15,1005,56,21,56]]
+    stul = []
+    for x in alldata:
+        stu = students()
+        stu.name = x[0]
+        stu.repo_name = x[1]
+        stu.numOfOpenPr = x[2]
+        stu.numOfClosedPr = x[3]
+        stu.numOfLinesAdded = x[4]
+        stu.totalCommits = x[5]
+        stu.numOfCreatedIssue = x[6]
+        stul.append(stu)
+    return stul
+
+# alldata a list of object containg all data of a participant 
+#object --> name -- repo_name -- total_commits -- openPRs -- closed PR -- total_additions -- total_deletions etc.
+
+def sortByAlpha(alldata):
+    return sorted(alldata, key=lambda l:l.name)
+
+def sortByOpenPr(alldata):
+    return sorted(alldata, key=lambda l:l.numOfOpenPr)
+
+def sortByClosedPr(alldata):
+    return sorted(alldata,key=lambda l:l.numOfClosedPr)
+
+def sortByTotalNumOfLines(alldata):
+    return sorted(alldata, key=lambda l:l.numOfLinesAdded)
+
+def printing(alldata):
+    for x in alldata:
+        print str(x.name) + " --  " +str(x.repo_name) +" -- "+str(x.totalCommits)+" --  "+str(x.numOfOpenPr)+" --  "+str(x.numOfClosedPr)+" -- "+str(x.numOfLinesAdded)+" -- "+str(x.numOfCreatedIssue)+"\n"
+
 if __name__ == '__main__':
-    print get_commits('defcon-007', 'defcon-007/utilobot')
+    alldata = students_list()
+    print "name____repoName____commits____openPR____closedPr____lineAdded____Issue"
+    print "____sort by name____"
+    printing(sortByAlpha(alldata)) 
+    print "____sort by openPr____"
+    printing(sortByOpenPr(alldata))
+    print "____sort by lines added____"
+    printing(sortByTotalNumOfLines(alldata))
+    print "____sort by closedPr____"
+    printing(sortByClosedPr(alldata))
+
+
---
+++ students.py
@@ -0,0 +1,10 @@
+class students:
+    def __init__(self, name = "XXX", repo_name = "ABC", totalCommits = 0, numOfOpenPr = 0, numOfClosedPr = 0, numOfCreatedIssue = 0, numOfLinesAdded = 0):
+        self.name = name
+        self.repo_name = repo_name
+        self.totalCommits = totalCommits
+        self.numOfOpenPr = numOfOpenPr
+        self.numOfClosedPr = numOfClosedPr
+        self.numOfCreatedIssue = numOfCreatedIssue
+        self.numOfLinesAdded = numOfLinesAdded
+
